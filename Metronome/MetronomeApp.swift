aa2cf3575302cd9d9aa65875f04ed845ece048b1
Aung Ko Min <jonahaung@gmail.com>
2022-01-17 23:29:28 +0800

    Initial Commit

---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Aung Ko Min on 17/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
